2a50b76781df1a9b9f8201a11b460d357e2db9ad
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-28 19:55:10 -0400

    Update optuna_grids.py

--- src/umdalib/training/ml_utils/optuna_grids.py
+++ src/umdalib/training/ml_utils/optuna_grids.py
@@ -46,3 +46,135 @@ def lgbm_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
         "reg_lambda": trial.suggest_loguniform("reg_lambda", 1e-8, 1.0),
     }
     return params
+
+
+# generate param grid for the sklearn models
+# "linear_regression", "ridge", "svr", "knn", "rfr", "gbr", "gpr"
+
+
+def linear_regression_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {}
+    return params
+
+
+def ridge_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "alpha": trial.suggest_loguniform("alpha", 1e-8, 1.0),
+        "solver": trial.suggest_categorical(
+            "solver", ["auto", "svd", "cholesky", "lsqr", "sparse_cg", "sag", "saga"]
+        ),
+    }
+    return params
+
+
+def svr_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "C": trial.suggest_loguniform("C", 1e-8, 1.0),
+        "kernel": trial.suggest_categorical(
+            "kernel", ["linear", "poly", "rbf", "sigmoid"]
+        ),
+        "degree": trial.suggest_int("degree", 1, 5),
+        "gamma": trial.suggest_categorical("gamma", ["scale", "auto"]),
+    }
+    return params
+
+
+def knn_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "n_neighbors": trial.suggest_int("n_neighbors", 1, 100),
+        "weights": trial.suggest_categorical("weights", ["uniform", "distance"]),
+        "algorithm": trial.suggest_categorical(
+            "algorithm", ["auto", "ball_tree", "kd_tree", "brute"]
+        ),
+        "leaf_size": trial.suggest_int("leaf_size", 1, 100),
+    }
+    return params
+
+
+def rfr_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
+        "max_depth": trial.suggest_int("max_depth", 1, 9),
+        "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),
+        "max_features": trial.suggest_categorical(
+            "max_features", ["auto", "sqrt", "log2"]
+        ),
+        "bootstrap": trial.suggest_categorical("bootstrap", [True, False]),
+    }
+    return params
+
+
+def gbr_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
+        "max_depth": trial.suggest_int("max_depth", 1, 9),
+        "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),
+        "max_features": trial.suggest_categorical(
+            "max_features", ["auto", "sqrt", "log2"]
+        ),
+        "subsample": trial.suggest_uniform("subsample", 0.5, 1.0),
+        "learning_rate": trial.suggest_loguniform("learning_rate", 1e-3, 1.0),
+    }
+    return params
+
+
+def gpr_param_grid(trial: optuna.Trial) -> Dict[str, Any]:
+    params = {
+        "kernel": trial.suggest_categorical(
+            "kernel",
+            [
+                "rbf",
+                "matern",
+                "rational_quadratic",
+                "exponential",
+                "dot_product",
+                "white",
+            ],
+        ),
+        "alpha": trial.suggest_loguniform("alpha", 1e-8, 1.0),
+        "optimizer": trial.suggest_categorical(
+            "optimizer",
+            [
+                "fmin_l_bfgs_b",
+                "fmin_ncg",
+                "simplex",
+                "cobyla",
+                "powell",
+                "bfgs",
+                "conjugate_gradient",
+                "newton_cg",
+                "trust_ncg",
+                "dogleg",
+                "trust_krylov",
+                "trust_region",
+            ],
+        ),
+    }
+    return params
+
+
+def get_param_grid(model_name: str) -> Dict[str, Any]:
+    if model_name == "xgboost":
+        return xgboost_param_grid
+    elif model_name == "catboost":
+        return catboost_param_grid
+    elif model_name == "lgbm":
+        return lgbm_param_grid
+    elif model_name == "linear_regression":
+        return linear_regression_param_grid
+    elif model_name == "ridge":
+        return ridge_param_grid
+    elif model_name == "svr":
+        return svr_param_grid
+    elif model_name == "knn":
+        return knn_param_grid
+    elif model_name == "rfr":
+        return rfr_param_grid
+    elif model_name == "gbr":
+        return gbr_param_grid
+    elif model_name == "gpr":
+        return gpr_param_grid
+    else:
+        raise ValueError(f"Model {model_name} not supported")
