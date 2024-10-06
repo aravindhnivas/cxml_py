a1d6bac9a5f3bde5ccaab0bcf921c51231280e1c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-06 14:30:08 -0400

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -1,9 +1,11 @@
 import umdalib.server as server
 
+
 class Args:
     port = 18704
     debug = False
-    
+
+
 args = Args()
-print(f'port: {args.port}, debug: {args.debug}')
+print(f"port: {args.port}, debug: {args.debug}")
 server.main(args)
