508c65cc96d523f6086d8f33ea2a7e15d9db2976
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-22 09:55:25 -0400

    Update __init__.py

--- src/umdalib/server/__init__.py
+++ src/umdalib/server/__init__.py
@@ -13,11 +13,18 @@ class Args:
 
 
 def main(args: Args):
-    logger.info(f"Starting server on port {args.port}")
-    if args.debug:
-        app.run(port=args.port, debug=True)
-        logger.warning("Server running in debug mode")
-        return
+    try:
+        logger.info(f"Starting server on port {args.port}")
+        if args.debug:
+            app.run(port=args.port, debug=True)
+            logger.warning("Server running in debug mode")
+            return
 
-    logger.info("Server running in production mode")
-    waitress.serve(app, port=args.port, url_scheme="http")
+        logger.info("Server running in production mode")
+        # raise NotImplementedError("Production mode not implemented yet")
+        waitress.serve(app, port=args.port, url_scheme="http")
+    except Exception as e:
+        logger.error(e)
+        raise
+    finally:
+        logger.warning("Server stopped")
