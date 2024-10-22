61c8309413218146c36827b18524b25f900083dd
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-22 11:47:50 -0400

    Update worker.py

--- src/umdalib/worker.py
+++ src/umdalib/worker.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import subprocess
 from redis import Redis
 from rq import Worker, Queue, Connection
 import gevent.monkey
@@ -9,7 +11,14 @@ listen = ["default"]
 redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 # Disable fork safety
-os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
+# os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
+
+# Check if the environment variable is set
+if os.getenv("OBJC_DISABLE_INITIALIZE_FORK_SAFETY") != "YES":
+    # Set the environment variable and restart the script
+    os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
+    subprocess.run([sys.executable] + sys.argv)
+    sys.exit()
 
 conn = Redis.from_url(redis_url)
 
