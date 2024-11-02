4c392117c9e131d8c1837cb48f92e759804c3453
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-11-02 13:38:40 -0400

    Update embedd_data.py

--- src/umdalib/training/embedd_data.py
+++ src/umdalib/training/embedd_data.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path as pt
 from time import perf_counter
-from typing import Callable, Literal, List
+from typing import Callable, Literal, List, Union
 
 import numpy as np
 from dask import array as da
@@ -18,14 +18,28 @@ import mapply
 mapply.init(n_workers=-1, chunk_size=100, max_chunks_per_worker=10, progressbar=True)
 
 
+def convert_to_float(value: Union[str, float]) -> float:
+    try:
+        return float(value)
+    except ValueError:
+        if isinstance(value, str) and "-" in value:
+            parts = value.split("-")
+            if len(parts) == 2 and parts[0] and parts[1]:
+                try:
+                    return (float(parts[0]) + float(parts[1])) / 2
+                except ValueError:
+                    pass
+        return np.nan
+
+
 @dataclass
 class Args:
     filename: str
     filetype: str
     key: str
     npartitions: int
-    mol2vec_dim: int
-    df_column: str
+    columnX: str
+    columnY: str
     embedding: Literal["VICGAE", "mol2vec"]
     pretrained_model_location: str
     test_mode: bool
@@ -34,6 +48,7 @@ class Args:
     embedd_savefile: str
     vectors_file: str
     use_dask: bool
+    index_col: str
 
 
 def VICGAE2vec(smi: str, model):
@@ -177,6 +192,9 @@ def main(args: Args):
 
     ddf = read_as_ddf(args.filetype, args.filename, args.key, use_dask=args.use_dask)
 
+    if args.index_col:
+        ddf = ddf.set_index(args.index_col)
+
     if args.use_dask:
         logger.info(f"{args.npartitions=}")
         ddf = ddf.repartition(npartitions=args.npartitions)
@@ -189,15 +207,15 @@ def main(args: Args):
         raise ValueError(f"Unknown embedding model: {args.embedding}")
 
     if args.use_dask:
-        vectors = ddf[args.df_column].apply(
+        vectors = ddf[args.columnX].apply(
             smi_to_vector, args=(model,), meta=(None, np.float32)
         )
     else:
         # for some reason, mapply is not faster with mol2vec embeddings
         if args.embedding == "mol2vec":
-            vectors = ddf[args.df_column].apply(smi_to_vector, args=(model,))
+            vectors = ddf[args.columnX].apply(smi_to_vector, args=(model,))
         else:
-            vectors = ddf[args.df_column].mapply(smi_to_vector, args=(model,))
+            vectors = ddf[args.columnX].mapply(smi_to_vector, args=(model,))
 
     if vectors is None:
         raise ValueError(f"Unknown embedding model: {args.embedding}")
@@ -210,37 +228,47 @@ def main(args: Args):
     logger.info(f"{vectors_file=}")
     logger.info(f"Begin computing embeddings for {fullfile.stem}...")
     time = perf_counter()
-
+    y = ddf[args.columnY]
+    y = y.apply(convert_to_float)
     vec_computed: np.ndarray = None
+
     with ProgressBar():
-        if args.use_dask and isinstance(vectors, da.Array):
-            vec_computed = vectors.compute()
+        if args.use_dask:
+            if isinstance(vectors, da.Array):
+                vec_computed = vectors.compute()
+            y = y.compute()
         else:
             vec_computed = vectors
 
         logger.info(f"{vec_computed.shape=}\n{vec_computed[0].shape=}")
         vec_computed = np.vstack(vec_computed)
+
         np.save(vectors_file, vec_computed)
         logger.success(f"Embedded numpy array saved to {vectors_file}")
 
     logger.info(f"{vec_computed.shape=}")
 
-    invalid_embedding_indices = [
-        i for i, arr in enumerate(vec_computed) if np.all(arr == 0)
-    ]
-    invalid_smiles = ddf[args.df_column].iloc[invalid_embedding_indices].tolist()
+    invalid_indices_full = ["# Invalid SMILES"]
+    invalid_vec_ind: np.ndarray[bool] = np.all(vec_computed == 0, axis=1)
+    if invalid_vec_ind.sum() > 0:
+        invalid_smiles: pd.Series = ddf[args.columnX].loc[invalid_vec_ind]
+        invalid_smiles_filename = vectors_file.with_suffix(".invalid_embeddings.csv")
+        invalid_smiles.to_csv(invalid_smiles_filename)
+        invalid_indices_full.extend(invalid_smiles.index.values)
 
-    # \xa0 is a non-breaking space in Latin1 (ISO 8859-1), also known as NBSP in Unicode. It's a character that prevents an automatic line break at its position. In HTML, it's often used to create multiple spaces that are visible.
-    invalid_smiles = [smiles.replace("\xa0", "").strip() for smiles in invalid_smiles]
+    invalid_y_ind = y.isna()
+    if invalid_y_ind.sum() > 0:
+        invalid_indices_full.extend(["# Invalid y values"])
+        invalid_y = y[invalid_y_ind]
+        invalid_y_filename = vectors_file.with_suffix(".invalid_y.csv")
+        invalid_y.to_csv(invalid_y_filename)
+        logger.info(f"Invalid Y values saved to {invalid_y_filename}")
+        invalid_indices_full.extend(invalid_y.index.values)
 
-    if len(invalid_embedding_indices) != len(invalid_smiles):
-        raise ValueError("Mismatch in number of invalid indices and SMILES")
-
-    invalid_df = pd.DataFrame(
-        {"InvalidIndex": invalid_embedding_indices, "SMILES": invalid_smiles}
-    )
-    invalid_smiles_filename = vectors_file.with_suffix(".invalid_embeddings.csv")
-    invalid_df.to_csv(invalid_smiles_filename, index=False)
+    if len(invalid_indices_full) > 0:
+        # invalid_indices = np.append(invalid_smiles.index.values, invalid_y.index.values)
+        invalid_indices_full_filename = vectors_file.with_suffix(".invalid_indices.txt")
+        np.savetxt(invalid_indices_full_filename, invalid_indices_full, fmt="%s")
 
     logger.info(
         f"Embeddings computed in {(perf_counter() - time):.2f} s and saved to {vectors_file.name}"
@@ -254,7 +282,7 @@ def main(args: Args):
         "filetype": args.filetype,
         "key": args.key,
         "npartitions": args.npartitions,
-        "df_column": args.df_column,
+        "columnX": args.columnX,
         "data_shape": vec_computed.shape,
         "invalid_smiles": len(invalid_smiles),
         "invalid_smiles_file": str(invalid_smiles_filename),
