5cf0eb66f5f5beb3a1635cfe5e383b3dcb9b72ef
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-11-24 18:18:01 -0500

    umap viz

--- notebooks/load_data.py
+++ notebooks/load_data.py
@@ -10,16 +10,29 @@ root_loc = (
 base_loc1 = root_loc / "[PHYSICAL CONSTANTS OF ORGANIC COMPOUNDS]"
 base_loc2 = root_loc / "[CRITICAL CONSTANTS OF ORGANIC COMPOUNDS]"
 
-processed_data_dirs = [
-    base_loc1
-    / "tmp_C_processed_data/analysis_data/filtered/tmpC_topelements_processed_data",
-    base_loc1
-    / "tbp_C_processed_data/analysis_data/filtered/tbp_topelements_processed_data",
-    base_loc1
-    / "vp_kPa_25C_filtered_ydata_processed_data/analysis_data/filtered/vp_kPa_25C_topelements_processed_data",
-    base_loc2 / "Pc_MPa_processed_data",
-    base_loc2 / "Tc_K_processed_data",
+
+saved_states_1 = [
+    "tmp_C_processed_data/analysis_data/filtered/tmpC_topelements_processed_data",
+    "tbp_C_processed_data/analysis_data/filtered/tbp_topelements_processed_data",
+    "vp_kPa_25C_filtered_ydata_processed_data/analysis_data/filtered/vp_kPa_25C_topelements_processed_data",
+    
 ]
+saved_states_2 = ["Pc_MPa_processed_data", "Tc_K_processed_data"]
+saved_states = saved_states_1 + saved_states_2
+
+processed_data_dirs = [base_loc1 / f for f in saved_states_1] + [base_loc2 / f for f in saved_states_2]
+embedded_vectors_dir = [d / "embedded_vectors" for d in processed_data_dirs]
+
+# processed_data_dirs = [
+#     base_loc1
+#     / "tmp_C_processed_data/analysis_data/filtered/tmpC_topelements_processed_data",
+#     base_loc1
+#     / "tbp_C_processed_data/analysis_data/filtered/tbp_topelements_processed_data",
+#     base_loc1
+#     / "vp_kPa_25C_filtered_ydata_processed_data/analysis_data/filtered/vp_kPa_25C_topelements_processed_data",
+#     base_loc2 / "Pc_MPa_processed_data",
+#     base_loc2 / "Tc_K_processed_data",
+# ]
 
 plots_dir = root_loc / "plots"
 titles = ["MP", "BP", "VP", "CP", "CT"]
