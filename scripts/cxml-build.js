eefebbd3a269149a4021f7fc4e99702bfebc1723
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-05-29 16:37:34 -0400

    Update cxml-build.js

--- scripts/cxml-build.js
+++ scripts/cxml-build.js
@@ -1,13 +1,14 @@
 import { spawn } from 'child_process'
 import path from 'path'
 import { $ } from "bun";
+import fs from 'fs'
 
 try {
     await $`rm -rf build`
     await $`rm -rf dist`
     await $`rm cxml_py.spec`    
 } catch (error) {
-    console.log('No build or dist directory')
+    console.log('No build or dist directory', error)
 }
 
 
@@ -31,8 +32,17 @@ const args =
 console.log(args)
 
 const py = spawn("pyinstaller", args)
-py.stdout.on('data', (data) => console.log(data.toString('utf8')))
-py.stderr.on('data', (data) => console.log(data.toString('utf8')))
+
+// save stdout and stderr to files
+const stdout_file = path.join('stdout.txt')
+const stderr_file = path.join('stderr.txt')
+
+py.stdout.pipe(fs.createWriteStream(stdout_file))
+py.stderr.pipe(fs.createWriteStream(stderr_file))
+
+// py.stdout.on('data', (data) => console.log(data.toString('utf8')))
+// py.stderr.on('data', (data) => console.log(data.toString('utf8')))
+
 py.on('close', async () => {
     const time_in_ms = new Date() - start_time
     const time_in_minutes = time_in_ms / 60000
